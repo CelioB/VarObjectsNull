24390e7f58e9ac2b9ea32151f5b4049827e51472
CelioB <celiogithubdev@gmail.com>
2025-07-23 07:11:44 -0300

    Add files via upload

---
+++ VarObjectsNull/VarObjectsNull/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace VarObjectsNull;
+
+using System;
+class Program
+{
+    string none = "Welisson Alysson";
+    int meuNumero = 7;
+    var nome2 = "Welisson Alysson";
+    var variavel = DateTime.Now;
+    Console.WriteLine("Hello World!");
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine(none);
+        Console.WriteLine(nome2);
+        Console.WriteLine(variavel);
+        Console.WriteLine(meuNumero);
+
+        // Variável do tipo var não pode ser nula
+
+        int? numeroNulo = null;
+        int minhaIdade = numeroNulo.Value;
+        bool informouIdade = numeroNulo.HasValue;
+
+        if (informouIdade)
+        {
+            Console.WriteLine($"Minha idade é {minhaIdade}");
+        }
+        else
+        {
+            Console.WriteLine("Você não informou a idade");
+        }
+    }
+
+
+}
